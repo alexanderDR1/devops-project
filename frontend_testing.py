d3b00c847fe9b8055976ac05965170e5e53c960b
alexanderDR1 <61018587+alexanderDR1@users.noreply.github.com>
2021-11-02 16:36:51 +0200

    Add files via upload

---
+++ frontend_testing.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#driver = webdriver.chrome(r"C:\Users\MY PC\OneDrive\Documents\WebDrivers\chromedriver")
+#drive.get('http://localhost:5000/users/<USER_ID>')
+
+
+#searchBox = driver.findElement(By.name("USER_ID"))
+
+#search_box.send_keys("webdriver")
+
