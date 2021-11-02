6b2b80cc5d26b64ab02e9ab9bd27d6e8c0dd2047
alexanderDR1 <61018587+alexanderDR1@users.noreply.github.com>
2021-11-02 16:37:42 +0200

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.chrome(r"C:\Users\MY PC\OneDrive\Documents\WebDrivers\chromedriver")
-drive.get('http://localhost:5000/users/<USER_ID>')
+drive.get('http://localhost:5000/users')
 
 
 #searchBox = driver.findElement(By.name("USER_ID"))
